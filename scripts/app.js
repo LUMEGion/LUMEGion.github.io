ca74896c538f9a92bff26b7a0a9cf011b8381ab3
Kevin Tuazon <kevintuazon45@gmail.com>
2021-03-04 23:54:54 +0800

    updates

--- scripts/app.js
+++ scripts/app.js
@@ -16,119 +16,6 @@ stop.disabled = true;
 let audioCtx;
 const canvasCtx = canvas.getContext("2d");
 
-//main block for doing the audio recording
-
-if (navigator.mediaDevices.getUserMedia) {
-  console.log("getUserMedia supported.");
-
-  const constraints = { audio: true };
-  let chunks = [];
-
-  let onSuccess = function (stream) {
-    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-      console.log("enumerateDevices() not supported.");
-      return false;
-    }
-
-    const mediaRecorder = new MediaRecorder(stream);
-
-    visualize(stream);
-
-    record.onclick = function () {
-      mediaRecorder.start();
-      console.log(mediaRecorder.state);
-      console.log("recorder started");
-      record.style.background = "red";
-
-      stop.disabled = false;
-      record.disabled = true;
-    };
-
-    stop.onclick = function () {
-      mediaRecorder.stop();
-      console.log(mediaRecorder.state);
-      console.log("recorder stopped");
-      record.style.background = "";
-      record.style.color = "";
-      // mediaRecorder.requestData();
-
-      stop.disabled = true;
-      record.disabled = false;
-    };
-
-    mediaRecorder.onstop = function (e) {
-      console.log("data available after MediaRecorder.stop() called.");
-
-      const clipName = prompt(
-        "Enter a name for your sound clip?",
-        "My unnamed clip"
-      );
-
-      const clipContainer = document.createElement("article");
-      const clipLabel = document.createElement("p");
-      const audio = document.createElement("audio");
-      const deleteButton = document.createElement("button");
-
-      clipContainer.classList.add("clip");
-      audio.setAttribute("controls", "");
-      deleteButton.textContent = "Delete";
-      deleteButton.className = "delete";
-
-      if (clipName === null) {
-        clipLabel.textContent = "My unnamed clip";
-      } else {
-        clipLabel.textContent = clipName;
-      }
-
-      clipContainer.appendChild(audio);
-      clipContainer.appendChild(clipLabel);
-      clipContainer.appendChild(deleteButton);
-      soundClips.appendChild(clipContainer);
-
-      audio.controls = true;
-      const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
-      chunks = [];
-      const audioURL = window.URL.createObjectURL(blob);
-      audio.src = audioURL;
-      console.log("recorder stopped");
-
-      deleteButton.onclick = function (e) {
-        let evtTgt = e.target;
-        evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-      };
-
-      clipLabel.onclick = function () {
-        const existingName = clipLabel.textContent;
-        const newClipName = prompt("Enter a new name for your sound clip?");
-        if (newClipName === null) {
-          clipLabel.textContent = existingName;
-        } else {
-          clipLabel.textContent = newClipName;
-        }
-      };
-    };
-
-    mediaRecorder.ondataavailable = function (e) {
-      chunks.push(e.data);
-    };
-
-    navigator.mediaDevices
-      .enumerateDevices()
-      .then(gotDevices)
-      .catch((err) => {
-        console.log("error getting MediaDeviceInfo list", err);
-      });
-  };
-
-  let onError = function (err) {
-    console.log("The following error occured: " + err);
-  };
-
-  navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
-} else {
-  console.log("getUserMedia not supported on your browser!");
-}
-
 function visualize(stream) {
   if (!audioCtx) {
     audioCtx = new AudioContext();
@@ -227,6 +114,7 @@ function gotDevices(deviceInfos) {
     }
   });
 }
+
 navigator.mediaDevices
   .enumerateDevices()
   .then(gotDevices)
@@ -234,29 +122,126 @@ navigator.mediaDevices
     console.log("error getting MediaDeviceInfo list", err);
   });
 
-function gotStream(stream) {
-  window.stream = stream; // make stream available to console
-  // Refresh button list in case labels have become available
-  return navigator.mediaDevices.enumerateDevices();
-}
-
 function start() {
   const audioSource = audioInputSelect.value;
   const constraints = {
     audio: { deviceId: audioSource ? { exact: audioSource } : undefined },
   };
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(gotStream)
-    .then(gotDevices)
-    .catch((err) => console.log(err));
+
+  //main block for doing the audio recording
+  if (navigator.mediaDevices.getUserMedia) {
+    console.log("getUserMedia supported.");
+
+    // const constraints = { audio: true };
+    let chunks = [];
+
+    let onSuccess = function (stream) {
+      const mediaRecorder = new MediaRecorder(stream);
+
+      visualize(stream);
+
+      record.onclick = function () {
+        mediaRecorder.start();
+        console.log(mediaRecorder.state);
+        console.log("recorder started");
+        record.style.background = "red";
+
+        stop.disabled = false;
+        record.disabled = true;
+      };
+
+      stop.onclick = function () {
+        mediaRecorder.stop();
+        console.log(mediaRecorder.state);
+        console.log("recorder stopped");
+        record.style.background = "";
+        record.style.color = "";
+        // mediaRecorder.requestData();
+
+        stop.disabled = true;
+        record.disabled = false;
+      };
+
+      mediaRecorder.onstop = function (e) {
+        console.log("data available after MediaRecorder.stop() called.");
+
+        const clipName = prompt(
+          "Enter a name for your sound clip?",
+          "My unnamed clip"
+        );
+
+        const clipContainer = document.createElement("article");
+        const clipLabel = document.createElement("p");
+        const audio = document.createElement("audio");
+        const deleteButton = document.createElement("button");
+
+        clipContainer.classList.add("clip");
+        audio.setAttribute("controls", "");
+        deleteButton.textContent = "Delete";
+        deleteButton.className = "delete";
+
+        if (clipName === null) {
+          clipLabel.textContent = "My unnamed clip";
+        } else {
+          clipLabel.textContent = clipName;
+        }
+
+        clipContainer.appendChild(audio);
+        clipContainer.appendChild(clipLabel);
+        clipContainer.appendChild(deleteButton);
+        soundClips.appendChild(clipContainer);
+
+        audio.controls = true;
+        const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+        chunks = [];
+        const audioURL = window.URL.createObjectURL(blob);
+        audio.src = audioURL;
+        console.log("recorder stopped");
+
+        deleteButton.onclick = function (e) {
+          let evtTgt = e.target;
+          evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+        };
+
+        clipLabel.onclick = function () {
+          const existingName = clipLabel.textContent;
+          const newClipName = prompt("Enter a new name for your sound clip?");
+          if (newClipName === null) {
+            clipLabel.textContent = existingName;
+          } else {
+            clipLabel.textContent = newClipName;
+          }
+        };
+      };
+
+      mediaRecorder.ondataavailable = function (e) {
+        chunks.push(e.data);
+      };
+
+      navigator.mediaDevices
+        .enumerateDevices()
+        .then(gotDevices)
+        .catch((err) => {
+          console.log("error getting MediaDeviceInfo list", err);
+        });
+    };
+
+    let onError = function (err) {
+      console.log("The following error occured: " + err);
+    };
+
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(onSuccess, onError)
+      .then(async () => navigator.mediaDevices.enumerateDevices)
+      .then(gotDevices)
+      .catch((err) => console.log(err));
+
+    // navigator.mediaDevices.getUserMedia(constraints);
+  } else {
+    console.log("getUserMedia not supported on your browser!");
+  }
 }
 
 audioInputSelect.onchange = start;
-
-// setInterval(async () => {
-//   await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-//   let devices = await navigator.mediaDevices
-//     .enumerateDevices()
-//     .then(gotDevices);
-// }, 3000);
+start();
